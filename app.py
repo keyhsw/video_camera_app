ba714998b38cc9bc02219d8783d560db3da38c9c
Ivan Reyes <Ivanrs@users.noreply.huggingface.co>
2022-11-11 23:30:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import gradio as gr
-
+import numpy as np
 
 def greet(name):
-    return "Hello " + name + "!!"
+    array = np.array([1, 2, 3])
+    return "Hello " + name + str(array) +  "!!"
 
 iface = gr.Interface(fn=greet, inputs="text", outputs="text")
 iface.launch()
+
