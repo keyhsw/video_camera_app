c25244c1212c6fc424ccb233aa6a610a82328149
Ivan Reyes <Ivanrs@users.noreply.huggingface.co>
2022-11-11 23:55:48 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import gradio as gr
 import cv2
 
-def gray(image):
+def gray(video):
+    vidcap = cv2.VideoCapture(video)
+    success,image = vidcap.read()
     grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     return grayscale
-    
-iface = gr.Interface(fn=gray, inputs="image", outputs="image")
+
+iface = gr.Interface(
+    fn=gray,
+    inputs=gr.Video(source="webcam", format="mp4"),
+    outputs="image"
+)
 iface.launch()
 
